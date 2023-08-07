ba32f56fc74c4fa8664b0a876366ab9028b2461b
fadlyian <mansyuraw24@gmail.com>
2023-08-07 20:53:33 +0700

    belajar memahami css styling, layout component, dan global styles[3~

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import Layouts from "../../components/layouts";
 
 export default function FirstPost() {
   const YourComponent = () => (
@@ -14,24 +15,27 @@ export default function FirstPost() {
 
   return (
     <>
-      <Head>
+      <Layouts>
+        {/* <Head>
+          <h1>First Post</h1>
+        </Head> */}
         <h1>First Post</h1>
-      </Head>
-      <h2>
-        <Link href="/">Back to Home</Link>
-      </h2>
-      <br />
-      <p>image tanpa component library next</p>
-      <img src="/images/profile.jpg" alt="Your Name" />
+        <h2>
+          <Link href="/">Back to Home</Link>
+        </h2>
+        <br />
+        <p>image tanpa component library next</p>
+        <img src="/images/profile.jpg" alt="Your Name" />
 
-      <br />
-      <p>image menggunakan componen library next/image</p>
-      <Image
-        src={"/images/profile.jpg"}
-        width={144}
-        height={144}
-        alt="ianganteng"
-      ></Image>
+        <br />
+        <p>image menggunakan componen library next/image</p>
+        <Image
+          src={"/images/profile.jpg"}
+          width={144}
+          height={144}
+          alt="ianganteng"
+        ></Image>
+      </Layouts>
     </>
   );
 }
