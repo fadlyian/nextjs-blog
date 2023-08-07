257a4de1895f8a428ad898a172eab1688eaff168
fadlyian <mansyuraw24@gmail.com>
2023-08-07 19:44:22 +0700

    contoh penggunaan componen image dengan libray next dengan component html

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,13 +1,37 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
-export default function FirstPost(){
-    return (
-        <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to Home</Link>
-            </h2>
-        </>
-    )
-    
+export default function FirstPost() {
+  const YourComponent = () => (
+    <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  );
+
+  return (
+    <>
+      <Head>
+        <h1>First Post</h1>
+      </Head>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+      <br />
+      <p>image tanpa component library next</p>
+      <img src="/images/profile.jpg" alt="Your Name" />
+
+      <br />
+      <p>image menggunakan componen library next/image</p>
+      <Image
+        src={"/images/profile.jpg"}
+        width={144}
+        height={144}
+        alt="ianganteng"
+      ></Image>
+    </>
+  );
 }
